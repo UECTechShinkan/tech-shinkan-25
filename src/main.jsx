fa1f9a1e97c73feb9f33d5e470c1f8b5b30f955b
Fu11erene <tatsuya91uta4@gmail.com>
2025-04-04 15:28:02 +0900

    a

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router basename="/tech-shinkan-25">
+    <Router>
       <App />
     </Router>
   </StrictMode>
